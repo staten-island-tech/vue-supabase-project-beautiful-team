9279b5f53d75fec19d3e812aa1f1fb6095c3ce7a
evY000 <000evan001@gmail.com>
2024-04-04 21:12:04 -0400

    Finished Connection

--- poke-trade-game/src/App.vue
+++ poke-trade-game/src/App.vue
@@ -1,27 +1,19 @@
 <script setup>
- import { ref, onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supabaseClient'
-const PokeCards = "https://api.pokemontcg.io/v2/cards/"
-const GrabCards = await fetch(PokeCards);
-/* import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue' */
-const PokeCardJSON = await GrabCards.json();
+const countries = ref([])
+async function getCountries() {
+  const { data } = await supabase.from('countries').select()
+  countries.value = data
+}
 
-
-  const countries = ref([])
-
-  async function getCountries() {
-    const { data } = await supabase.from('countries').select()
-    countries.value = data
-  }
-
-  onMounted(() => {
-    getCountries()
-  })
-  console.log(data)
+onMounted(() => {
+  getCountries()
+})
 </script>
+
 <template>
-    <ul>
-      <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-    </ul>
-  </template>
+  <ul>
+    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+  </ul>
+</template>
---
+++ poke-trade-game/src/components/PokeApi.vue
@@ -0,0 +1,7 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue' 
+ const PokeCards = "https://api.pokemontcg.io/v2/cards/"
+const GrabCards = await fetch(PokeCards);
+const PokeCardJSON = await GrabCards.json(); 
+</script>
