839c1c369af4601489fcbca93928220d3440bc1a
evY000 <000evan001@gmail.com>
2024-04-05 13:11:50 -0400

    fixing bugs + combining stuff

--- poke-trade-game/src/App.vue
+++ poke-trade-game/src/App.vue
@@ -1,17 +1,31 @@
 <script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supabaseClient'
 const countries = ref([])
+
 async function getCountries() {
   const { data } = await supabase.from('countries').select()
   countries.value = data
 }
+ 
 
 onMounted(() => {
   getCountries()
 })
 </script>
-
+<script>
+const PokeCards = "https://api.pokemontcg.io/v2/cards/"
+const GrabCards = await fetch(PokeCards);
+const PokeCardJSON = await GrabCards.json(); 
+function pokemons(){
+    PokeCardJSON.forEach(card => {
+    const name = card.name
+    console.log(name)
+});}
+pokemons()
+</script>
 <template>
   <ul>
     <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
--- poke-trade-game/src/components/PokeApi.vue
+++ poke-trade-game/src/components/PokeApi.vue
@@ -1,13 +1,1 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue' 
- const PokeCards = "https://api.pokemontcg.io/v2/cards/"
-const GrabCards = await fetch(PokeCards);
-const PokeCardJSON = await GrabCards.json(); 
-PokeCardJSON.forEach(card => {
-    const name = card.name
-    console.log(name)
-});
-</script>
-<template>
-</template>
+<script></script>
