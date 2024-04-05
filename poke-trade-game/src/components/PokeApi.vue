3c12dbca6aba06a2c069bebb78faffbfbb584408
evY000 <000evan001@gmail.com>
2024-04-04 21:35:01 -0400

    stuff

--- poke-trade-game/src/components/PokeApi.vue
+++ poke-trade-game/src/components/PokeApi.vue
@@ -4,4 +4,10 @@ import HelloWorld from './components/HelloWorld.vue'
  const PokeCards = "https://api.pokemontcg.io/v2/cards/"
 const GrabCards = await fetch(PokeCards);
 const PokeCardJSON = await GrabCards.json(); 
+PokeCardJSON.forEach(card => {
+    const name = card.name
+    console.log(name)
+});
 </script>
+<template>
+</template>
