cacd596982b8ef260846b15a5394b1d73cc220ce
evY000 <000evan001@gmail.com>
2024-04-09 12:22:01 -0400

    testing

--- poke-trade-game/src/App.vue
+++ poke-trade-game/src/App.vue
@@ -3,31 +3,31 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supabaseClient'
-const countries = ref([])
+const test = ref([])
 
-async function getCountries() {
-  const { data } = await supabase.from('countries').select()
-  countries.value = data
+async function getTest() {
+  const { data } = await supabase.from('test').select()
+  test.value = data
 }
  
 
 onMounted(() => {
-  getCountries()
+  getTest()
 })
 </script>
 <script>
-const PokeCards = "https://api.pokemontcg.io/v2/cards/"
+/* const PokeCards = "https://api.pokemontcg.io/v2/cards/"
 const GrabCards = await fetch(PokeCards);
-const PokeCardJSON = await GrabCards.json(); 
-function pokemons(){
+const PokeCardJSON = await GrabCards.json();  */
+/* function pokemons(){
     PokeCardJSON.forEach(card => {
     const name = card.name
-    console.log(name)
-});}
-pokemons()
+    console.log(name) too laggy right now
+});} */
+/* pokemons() */
 </script>
 <template>
   <ul>
-    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+    <li v-for="name in test" :key="name.id">{{ name.name }}</li>
   </ul>
 </template>
