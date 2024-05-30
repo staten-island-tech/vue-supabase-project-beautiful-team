df1d735a94a1568169ae762e831e9b6502f05b7d
evY000 <000evan001@gmail.com>
2024-05-30 13:34:39 -0400

    stuff

--- poke-trade-game/src/views/Cards.vue
+++ poke-trade-game/src/views/Cards.vue
@@ -1,11 +1,8 @@
   <script>
-  <script type="text/javascript">
-    const Money = 100
-  </script>
+  
  </script> 
  <template>
         <RouterLink to="/"> Go To Home</RouterLink>
-        <h1> <script type="text/javascript">
-        document.write(Money)
-      </script> </h1>
+      
+     
  </template>
