e466a794be91e1bd38401a1161cb68ec08846f1a
evY000 <000evan001@gmail.com>
2024-05-31 14:28:05 -0400

    fix for each later

--- poke-trade-game/src/views/Cards.vue
+++ poke-trade-game/src/views/Cards.vue
@@ -1,25 +1,30 @@
+<template>
+       <RouterLink to="/"> Go To Home</RouterLink>
+       <div id="app"></div>
+ </template>
 <script>
 const DOMSelectors = {
-Thing : document.getElementById("body")}
-
-const Link = "https://pokeapi.co/api/v2/pokemon/"
+      App  : document.getElementById("app")
+}
+const Link = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+async function ShowImage(){
+       try {
+              
+}
+catch (error) {
+          console.log(error)}};
 async function GetInput(){
   try {
        const response = await fetch(Link);
        const stuff = await response.json();  
               stuff.results.forEach((card) => {
-               const FirstName = card.name
-               const cards = `<h1> ${FirstName} </h1>`
-                     console.log(FirstName)
-                     console.log(stuff)
-             DOMSelectors.Thing.insertAdjacentHTML('beforeend', cards)})}
-       catch(error) {
+                      const FirstName = card.name
+                   const Up = FirstName[0].toUpperCase() + FirstName.slice(1);
+                      const cards = `<h1> ${Up} </h1> <button onClick=ShowImage()> Click For Image </button>`
+                      DOMSelectors.App.insertAdjacentHTML('beforeend', cards)})}
+   catch(error) {
           console.log(error)
          
         }};
 GetInput()
 </script> 
- <template>
-        <RouterLink to="/"> Go To Home</RouterLink>
-     <body></body>
- </template>
