a8c3b61f616a336d627f09f832c42bb30dbaee90
evY000 <000evan001@gmail.com>
2024-05-31 13:32:35 -0400

    CARDS

--- poke-trade-game/src/views/Cards.vue
+++ poke-trade-game/src/views/Cards.vue
@@ -1,8 +1,25 @@
-  <script>
-  
- </script> 
+<script>
+const DOMSelectors = {
+Thing : document.getElementById("body")}
+
+const Link = "https://pokeapi.co/api/v2/pokemon/"
+async function GetInput(){
+  try {
+       const response = await fetch(Link);
+       const stuff = await response.json();  
+              stuff.results.forEach((card) => {
+               const FirstName = card.name
+               const cards = `<h1> ${FirstName} </h1>`
+                     console.log(FirstName)
+                     console.log(stuff)
+             DOMSelectors.Thing.insertAdjacentHTML('beforeend', cards)})}
+       catch(error) {
+          console.log(error)
+         
+        }};
+GetInput()
+</script> 
  <template>
         <RouterLink to="/"> Go To Home</RouterLink>
-      
-     
+     <body></body>
  </template>
